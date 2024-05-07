f201d58b69c93c8bfb09f19025e801a7ef518b3e
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-05-07 11:14:10 -0400

    womp womp

--- build.ts
+++ build.ts
@@ -1,3 +1,16 @@
 import esbuild from "esbuild"
-
-// TODO
+import { copyFile, mkdir, readFile } from 'node:fs/promises';
+await rimraf('dist');
+await mkdir('dist');
+await copyFile('src/config.js', 'dist/config.js');
+await build({
+    platform: 'browser',
+    sourcemap: true,
+    minify: !isDevelopment,
+    entryPoints: {
+        'bundle': './src/bundle.js',
+    },
+    bundle: true,
+    logLevel: 'info',
+    outdir: 'dist/',
+});
