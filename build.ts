7337d16e4eab68bdec1f9e30c673c34f36a3c309
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-05-07 11:34:57 -0400

    OK this time actually fix build

--- build.ts
+++ build.ts
@@ -1,6 +1,7 @@
 import { build } from "esbuild"
 import { rimraf } from "rimraf"
 import { copyFile, mkdir, readFile } from 'node:fs/promises';
+const isDevelopment = process.argv.includes('--dev');
 await rimraf('dist');
 await mkdir('dist');
 await copyFile('src/config.js', 'dist/config.js');
